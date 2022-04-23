232a9313d3edc18d49bc961ee309c8e983cbeca4
Eduardo Marques <emarques@outlook.com>
2022-04-23 05:23:07 +0000

    commit2

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,50 @@
 module.exports = app => {
     const Tasks = app.db.models.Tasks;
-    app.get("/tasks", (req, res) => {
-        Tasks.findAll({}).then(tasks => {
-            res.json({tasks: tasks});
+
+    app.route("/tasks")
+        // .all((req, res, next) => {
+        //     //Middleware de pré-execução das rotas
+        //     delete req.body.id;
+        //     next();
+        // })
+        .get((req, res) => {
+            // lista de tarefas
+            Tasks.findAll({})
+                .then(result => res.json(result))
+                .catch(error => {
+                    res.status(412).json({msg: error.message});
+                });
+        })
+        .post((req, res) => {
+            //cadastra nova tarefa
+            Tasks.create(req.body)
+            .then(result => res.json(result))
+            .catch(error => {
+                res.status(412).json({msg: error.message});
+            });
         });
-    });
-};
+
+    app.route("/tasks/:id")
+        // .all((req, res, next) => {
+        //     //Middleware de pré-execução das rotas
+        //     delete req.body.id;
+        //     next();
+        // })
+        .get((req, res) => {
+            // "/tasks/1" lista tarefa
+        })
+        .put((req, res) => {
+            // "/tasks/1" cadastra nova tarefa
+        })
+        .delete((req, res) => {
+            // "/tasks/1" exclui tarefa
+        });
+
+    };
+
+    // app.get("/tasks", (req, res) => {
+    //     Tasks.findAll({}).then(tasks => {
+    //         res.json({tasks: tasks});
+    //     });
+    // });
+
